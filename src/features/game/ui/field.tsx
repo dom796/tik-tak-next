683280663fe67a1ca4c0b621832ca1e68762fd87
dom796 <dom796@users.noreply.github.com>
2026-04-03 10:13:25 +0300

    fix: correct sybmol typo to symbol in GameField

--- src/features/game/ui/field.tsx
+++ src/features/game/ui/field.tsx
@@ -12,14 +12,14 @@ export function GameField({
 
   return (
     <div className="grid grid-cols-3">
-      {game.field.map((sybmol, index) => (
+      {game.field.map((symbol, index) => (
         <button
           onClick={() => onCellClick?.(index)}
           key={index}
-          disabled={!isInteractive || sybmol !== null}
+          disabled={!isInteractive || symbol !== null}
           className="border border-primary w-10 h-10 flex justify-center items-center disabled:opacity-60 disabled:cursor-not-allowed"
         >
-          {sybmol ?? ""}
+          {symbol ?? ""}
         </button>
       ))}
     </div>
