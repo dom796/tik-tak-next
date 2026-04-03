60f47688afd48fbbda1aa32d30241c30bb2278e6
dom796 <dom796@users.noreply.github.com>
2026-04-03 10:13:05 +0300

    fix: correct firtsPlayer typo to firstPlayer in GamePlayers

--- src/features/game/ui/players.tsx
+++ src/features/game/ui/players.tsx
@@ -3,13 +3,13 @@
 import { GameDomain } from "@/entities/game";
 
 export function GamePlayers({ game }: { game: GameDomain.GameEntity }) {
-  const firtsPlayer = game.status === "idle" ? game.creator : game.players[0];
+  const firstPlayer = game.status === "idle" ? game.creator : game.players[0];
   const secondPlayer = game.status === "idle" ? undefined : game.players[1];
 
   return (
     <div className="flex flex-row gap-4 justify-between">
       <div className="text-lg">
-        X - {firtsPlayer.login}:{firtsPlayer.rating}
+        X - {firstPlayer.login}:{firstPlayer.rating}
       </div>
       <div className="text-lg">
         O - {secondPlayer?.login ?? "..."}:{secondPlayer?.rating ?? "..."}
