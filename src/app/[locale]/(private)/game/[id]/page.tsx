b34b318e122e3a840172c8c524795066093f690d
dom796 <dom796@users.noreply.github.com>
2026-04-06 10:19:23 +0300

    style: add side padding and increase max-width on game page

--- src/app/[locale]/(private)/game/[id]/page.tsx
+++ src/app/[locale]/(private)/game/[id]/page.tsx
@@ -7,7 +7,7 @@ export default async function Page({
 }) {
   const { id } = await params;
   return (
-    <main className="flex flex-col grow justify-center py-8 w-full max-w-[400px] mx-auto">
+    <main className="flex flex-col grow justify-center py-8 px-4 w-full max-w-[400px] md:max-w-[480px] mx-auto">
       <Game gameId={id} />
     </main>
   );
