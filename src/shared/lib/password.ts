17a464c5d2d46d961fa77342dd76294ac3dbfcf9
dom796 <dom796@users.noreply.github.com>
2026-04-07 10:41:21 +0300

    refactor: remove unused removePassword utility

--- src/shared/lib/password.ts
+++
@@ -1,7 +0,0 @@
-export const removePassword = <T extends { passwordHash: string }>({
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  passwordHash: _,
-  ...rest
-}: T): Omit<T, "passwordHash"> => {
-  return rest;
-};
